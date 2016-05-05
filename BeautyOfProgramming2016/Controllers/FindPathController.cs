9de2b1f6e1cdf56db06e9db6ea7605c686cada2d
fcxxzux <fcxxzux@qq.com>
2016-05-05 16:59:28 +0800

    实现了求1hop路径的API，待优化

--- BeautyOfProgramming2016/Controllers/FindPathController.cs
+++ BeautyOfProgramming2016/Controllers/FindPathController.cs
@@ -11,14 +11,112 @@ using System.Web.Http;
 using System.IO;
 using System.Web;
 
-namespace BeautyOfProgramming2016.Controllers
-{
-    public class FindPathController : ApiController
-    {
-        public List<long[]> GetPath(long id1, long id2)
-        {
+namespace BeautyOfProgramming2016.Controllers {
+    public class FindPathController : ApiController {
+        public async Task<List<long[]>> GetPath(long id1, long id2) {
+            DateTime begin = DateTime.Now;
+            TimeSpan expectTime = new TimeSpan(0, 1, 0);
             List<long[]> ans = new List<long[]>();
-            ans.Add(new long[] { id1, id2 });
+            bool isId1Au = await IsPossible("composite(AA.AuId=" + id1 + ")"),
+                isId2Au = await IsPossible("composite(AA.AuId=" + id2 + ")");
+            string queryId1 = isId1Au ? ("composite(AA.AuId=" + id1 + ")") : ("Id=" + id1);
+            string queryId2 = isId2Au ? ("composite(AA.AuId=" + id2 + ")") : ("Id=" + id2);
+            AcademicQueryResponse aboutId1 = await DeserializedResult(queryId1); ;
+            Entity[] entitys = aboutId1.entities;
+
+            HashSet<long> dict = new HashSet<long>();
+
+            foreach (Entity x in entitys) {
+                if (ans.Count > 150) break;
+                if (isId1Au) {
+                    if (isId2Au) {
+                        foreach (Author y in x.AA) {
+                            //同文章下2个作者
+                            if (y.AuId == id2) {
+                                ans.Add(new long[] { id1, x.Id, id2 });
+                            }
+                            //同机构下2个作者
+                            if (y.AuId == id1) {
+                                if(dict.Contains(y.AfId))continue;
+                                dict.Add( y.AfId);
+                                bool res = await IsPossible("composite(AND(AA.AfId=" + y.AfId + ",AA.AuId="+id2 + ")");
+                                if (res) {
+                                    ans.Add(new long[] { id1, y.AfId, id2 });
+                                }
+                            }
+                            if (DateTime.Now-begin> expectTime) return ans;
+                        }
+                    }else {
+                        //这篇论文的引文 引用了id2论文
+                        foreach(long rid in x.RId) {
+                            if (dict.Contains( rid)) continue;
+                            dict.Add(rid);
+                            bool res = await IsPossible("AND(Id=" + rid + ",RId=" + id2 + ")");
+                            if (res) {
+                                ans.Add(new long[] { id1, rid, id2 });
+                            }
+                            if (DateTime.Now - begin > expectTime) return ans;
+                        }
+                        
+                    }
+                } else {
+                    if (isId2Au) {
+                        //这篇论文的引文 有作者id2
+                        foreach (long rid in x.RId) {
+                            if (dict.Contains(rid)) continue;
+                            dict.Add(rid);
+                            bool res = await IsPossible("AND(Id=" + rid + ",composite(AA.AuId=" + id2 + "))");
+                            if (res) {
+                                ans.Add(new long[] { id1, rid, id2 });
+                            }
+                            if (DateTime.Now - begin > expectTime) return ans;
+                        }
+                    }else {
+                        //这篇论文的引文 有引文id2
+                        foreach (long rid in x.RId) {
+                            if (dict.Contains(rid)) continue;
+                            dict.Add(rid);
+                            bool res = await IsPossible("AND(Id=" + rid + ",composite(AA.AuId=" + id2 + "))");
+                            if (res) {
+                                ans.Add(new long[] { id1, rid, id2 });
+                            }
+                            if (DateTime.Now - begin > expectTime) return ans;
+                        }
+                        //这篇论文的作者中 有人写了id2
+                        foreach (Author y in x.AA) {
+                            if (dict.Contains(y.AuId)) continue;
+                            dict.Add(y.AuId);
+                            bool res = await IsPossible("AND(composite(AA.AuId=" + y.AuId + "),Id=" + id2 + ")");
+                            if (res) {
+                                ans.Add(new long[] { id1, y.AuId, id2 });
+                            }
+                            if (DateTime.Now - begin > expectTime) return ans;
+                        }
+                        //这篇论文的所属期刊/所属会议/所属领域中 有id2
+                        if (x.C != null) {
+                            bool res = await IsPossible("AND(composite(C.CId=" + x.C.CId + "),Id=" + id2 + ")");
+                            if (res) {
+                                ans.Add(new long[] { id1, x.C.CId, id2 });
+                            }
+                        }
+                        if (x.J != null) {
+                            bool res = await IsPossible("AND(composite(J.JId=" + x.J.JId + "),Id=" + id2 + ")");
+                            if (res) {
+                                ans.Add(new long[] { id1, x.J.JId, id2 });
+                            }
+                        }
+                        if (x.F != null) {
+                            bool res = await IsPossible("AND(composite(F.FId=" + x.F.FId + "),Id=" + id2 + ")");
+                            if (res) {
+                                ans.Add(new long[] { id1, x.F.FId, id2 });
+                            }
+                        }
+                        if (DateTime.Now - begin > expectTime) return ans;
+                    }
+                }
+            }
+
+
             return ans;
         }
 
@@ -27,25 +125,23 @@ namespace BeautyOfProgramming2016.Controllers
         // 具体没深究，TODO
         // 参见
         // http://stackoverflow.com/questions/10343632/httpclient-getasync-never-returns-when-using-await-async
-        public async Task<AcademicQueryResponse> GetSpecificInfo(long entityid)
-        {
+        public async Task<AcademicQueryResponse> GetSpecificInfo(long entityid) {
             return await DeserializedResult("Id=" + entityid);
         }
 
-        private async Task<AcademicQueryResponse> DeserializedResult(string order){
-            //"Id=2140251882";
-            //"Composite(AA.AuN == 'jaime teevan')"
-            var response = await MakeRequest(order, 10000, "Id,RId,F.FId,C.CId,J.JId,AA.AuId,AA.AfId");
-            //while (!response.IsCompleted) ;
-            string result = response.Content.ReadAsStringAsync().Result;
-            var serializer = new DataContractJsonSerializer(typeof(AcademicQueryResponse));
-            var mStream = new MemoryStream(response.Content.ReadAsByteArrayAsync().Result);
-            AcademicQueryResponse readResult = (AcademicQueryResponse)serializer.ReadObject(mStream);
-            return readResult;
+        private async Task<bool> IsPossible(string expr) {
+            AcademicQueryResponse tmp = await DeserializedResult(expr,1);
+            return tmp.entities != null && tmp.entities.Length > 0;
         }
 
-        private async Task<HttpResponseMessage> MakeRequest(string expr, long count, string attributes)
-        {
+        private async Task<AcademicQueryResponse> DeserializedResult(
+            string expr,
+            long count= 10000,
+            string attributes= "Id,RId,F.FId,C.CId,J.JId,AA.AuId,AA.AfId"
+            ) {
+            //"Id=2140251882";
+            //"Composite(AA.AuN == 'jaime teevan')"
+
             var client = new HttpClient();
             var queryString = HttpUtility.ParseQueryString(string.Empty);
 
@@ -58,7 +154,11 @@ namespace BeautyOfProgramming2016.Controllers
             var uri = "https://oxfordhk.azure-api.net/academic/v1.0/evaluate?" + queryString;
 
             var response = await client.GetAsync(uri);
-            return response;
+            
+            var serializer = new DataContractJsonSerializer(typeof(AcademicQueryResponse));
+            var mStream = new MemoryStream(response.Content.ReadAsByteArrayAsync().Result);
+            AcademicQueryResponse readResult = (AcademicQueryResponse)serializer.ReadObject(mStream);
+            return readResult;
         }
     }
 }
